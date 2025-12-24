00f241301893e62acdd12b116c23c320cb57a696
jayfedora83-lab <jayfedora83@gmail.com>
2025-12-24 18:24:36 -0500

    Delete twrp_q6.mk

--- twrp_q6.mk
+++
@@ -1,7 +0,0 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-$(call inherit-product, vendor/twrp/config/common.mk)
-PRODUCT_DEVICE := q6
-PRODUCT_NAME := twrp_q6
-PRODUCT_BRAND := Facetel
-PRODUCT_MODEL := Q6
-PRODUCT_MANUFACTURER := Facetel
