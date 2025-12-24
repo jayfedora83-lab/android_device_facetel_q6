fb8790bf684f56a37246c918e9be2bf702b98932
jayfedora83-lab <jayfedora83@gmail.com>
2025-12-24 16:42:08 -0500

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_Pulla.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_Pulla-user \
+    omni_Pulla-userdebug \
+    omni_Pulla-eng
+
