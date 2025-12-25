8a687a7453c1bf4e36bd466fa147ccf3504fad81
jayfedora83-lab <jayfedora83@gmail.com>
2025-12-24 19:51:09 -0500

    Add files via upload

---
+++ device/hotpepper/Pulla/vendorsetup.sh
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+add_lunch_combo omni_Pulla-user
+add_lunch_combo omni_Pulla-userdebug
+add_lunch_combo omni_Pulla-eng
+
