0ca868617abd1a533549b004d4deb9082d5c3503
jayfedora83-lab <jayfedora83@gmail.com>
2025-12-24 20:40:19 -0500

    Update twrp_q6.mk

--- device/hotpepper/Pulla/twrp_q6.mk
+++ device/hotpepper/Pulla/twrp_q6.mk
@@ -1,7 +1,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
-PRODUCT_DEVICE := q6
-PRODUCT_NAME := twrp_q6
+
+PRODUCT_DEVICE := Pulla
+PRODUCT_NAME := twrp_Pulla
 PRODUCT_BRAND := Facetel
 PRODUCT_MODEL := Q6
 PRODUCT_MANUFACTURER := Facetel
