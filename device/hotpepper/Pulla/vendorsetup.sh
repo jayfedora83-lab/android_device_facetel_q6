7afe85f7ffa158446f45244cb690d4868a1c74a1
jayfedora83-lab <jayfedora83@gmail.com>
2025-12-24 20:43:41 -0500

    Update vendorsetup.sh

--- device/hotpepper/Pulla/vendorsetup.sh
+++ device/hotpepper/Pulla/vendorsetup.sh
@@ -1,9 +1,1 @@
-#
-# Copyright (C) 2025 The Android Open Source Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-add_lunch_combo omni_Pulla-user
-add_lunch_combo omni_Pulla-userdebug
-add_lunch_combo omni_Pulla-eng
-
+add_lunch_combo twrp_Pulla-eng
